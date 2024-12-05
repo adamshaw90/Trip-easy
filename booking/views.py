ecb47a9d617640ed128eaff0f5d936e2635d1358
Adam Standerwick-Shaw <adamoshaw90@gmail.com>
2024-12-05 07:56:10 +0000

    Create a new project called tripeasy, a new app called booking and install latest python package

---
+++ booking/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def my_booking(request):
+    return HttpResponse("This will be the booking page!")

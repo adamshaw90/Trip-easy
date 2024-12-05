12e55d4c7ed7e6fd620861782e1e7986dccc2089
Adam Standerwick-Shaw <adamoshaw90@gmail.com>
2024-12-05 09:12:40 +0000

    Add a env.py and import database url, create and add a secret key to add to env file and settings file

--- tripeasy/settings.py
+++ tripeasy/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b&85mb+cm@(h)c5r6bo9nd*c&5esc6sh&pwgv_m8d=8pa#o*^p'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

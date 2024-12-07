7048438ace011fa159ee19492a8a1ddb563efbf8
Adam Standerwick-Shaw <adamoshaw90@gmail.com>
2024-12-07 07:49:32 +0000

    Add the url patterns in urls.py with the location url now removed as it is not required

--- booking/urls.py
+++ booking/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),                 # Home page
-    path('about/', views.about, name='about'),         # About page
-    path('register/', views.register, name='register'),# Registration page
-    path('book/<int:pk>/', views.book_itinerary, name='book_itinerary'),
-    path('update/<int:pk>/', views.update_booking, name='update_booking'),
+    path('itinerary/book/<int:pk>/', views.book_itinerary, name='book_itinerary'),
+    path('profile/complete/', views.complete_profile, name='complete_profile'),
+    path('profile/update/', views.update_profile, name='update_profile'),
 ]

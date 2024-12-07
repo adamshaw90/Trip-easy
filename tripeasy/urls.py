7e5cf739f9ff46b4c031def0cf07c703bd780b3e
Adam Standerwick-Shaw <adamoshaw90@gmail.com>
2024-12-07 07:50:29 +0000

    Add the url patterns to the tripeasy urls

--- tripeasy/urls.py
+++ tripeasy/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('booking.urls')),  # Link to the app's URLs
+    path('', include('booking.urls')),
 ]

566a63d70cae8b175f35afc0c9cb60ac27f3ff64
Adam Standerwick-Shaw <adamoshaw90@gmail.com>
2024-12-07 08:23:49 +0000

    Install whitenoise and add to requirements.txt

--- booking/urls.py
+++ booking/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('booking', views.home, name='home'),
     path('itinerary/book/<int:pk>/', views.book_itinerary, name='book_itinerary'),
     path('profile/complete/', views.complete_profile, name='complete_profile'),
     path('profile/update/', views.update_profile, name='update_profile'),

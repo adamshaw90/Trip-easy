7b2487d01f032c563163fad90eb514dabea7a56d
Adam Standerwick-Shaw <adamoshaw90@gmail.com>
2024-12-07 06:19:33 +0000

    Add a location class to the models file, also add a static folder with css folder and style.css file inside. Add a templates folder with base.html file and some boilerplate html

--- booking/models.py
+++ booking/models.py
@@ -21,3 +21,13 @@ class Itinerary(models.Model):
 
     def __str__(self):
         return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    itinerary = models.ForeignKey(Itinerary, on_delete=models.CASCADE, related_name='locations')
+
+    def __str__(self):
+        return self.name

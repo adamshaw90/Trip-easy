4911fc0cf95a7cb915e4f60654f8bdcc14690e21
Adam Standerwick-Shaw <adamoshaw90@gmail.com>
2024-12-07 06:29:08 +0000

    Change debug status to false

--- tripeasy/settings.py
+++ tripeasy/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-adamshaw90-tripeasy-ixxqjrvi5au.ws.codeinstitute-ide.net','.herokuapp.com'
 ]

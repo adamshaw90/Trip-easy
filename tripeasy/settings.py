80c91e521fe16be25f49dbc8b7131e51cf845d66
Adam Standerwick-Shaw <adamoshaw90@gmail.com>
2024-12-28 10:08:28 +0000

    Add env.py file again

--- tripeasy/settings.py
+++ tripeasy/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-adamshaw90-tripeasy-qn717x1sv0o.ws.codeinstitute-ide.net','.herokuapp.com'
+ALLOWED_HOSTS = ['8000-adamshaw90-tripeasy-sazwp0empwc.ws.codeinstitute-ide.net','.herokuapp.com'
 ]
 
 
